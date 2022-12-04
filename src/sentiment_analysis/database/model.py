95dad30d6c3bd486f463427085918354e5b0f895
imilyutin <idmilyutin@gmail.com>
2022-12-05 00:55:27 +0400

    added unfinished tets on add_model

--- src/sentiment_analysis/database/model.py
+++ src/sentiment_analysis/database/model.py
@@ -1,5 +1,8 @@
 from datetime import datetime
+from typing import Union
+from collections.abc import Iterable
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import Session
 from sentiment_analysis.database.base import Base
 
 
@@ -15,12 +18,56 @@ class Model(Base):
 
 
 def add_model(
-    session,
-    model_name: str,
-    model_version: int,
-    model_desc: str,
-    model_ref: str,
-    train_size: int,
-    trained_at_dt: datetime
+    session: Session,
+    model_name: Union[str, Iterable[str]],
+    model_version: Union[int, Iterable[int]],
+    model_desc: Union[str, Iterable[str]],
+    model_ref: Union[str, Iterable[str]],
+    train_size: Union[int, Iterable[int]],
+    trained_at_dt: Union[datetime, Iterable[datetime]]
 ):
-    pass
+    args = [
+        model_name,
+        model_version,
+        model_desc,
+        model_ref,
+        train_size,
+        trained_at_dt
+    ]
+    if all(
+            isinstance(arg, Iterable) and not isinstance(arg, str)
+            for arg in args
+            ):
+        records = []
+        for nm, ver, desc, ref, size, dt in zip(*args):
+            record = Model(
+                model_name=nm,
+                model_version=ver,
+                model_desc=desc,
+                model_ref=ref,
+                train_size=size,
+                trained_at_dt=dt
+            )
+            records.append(record)
+        session.add_all(records)
+        session.commit()
+    elif all(
+            not isinstance(arg, Iterable) or isinstance(arg, str)
+            for arg in args
+            ):
+        records = Model(
+            model_name=model_name,
+            model_version=model_version,
+            model_desc=model_desc,
+            model_ref=model_ref,
+            train_size=train_size,
+            trained_at_dt=trained_at_dt
+        )
+        session.add(records)
+        session.commit()
+    else:
+        raise ValueError(
+            'All arguments (except session) must be either iterable or not'
+            ' iterable at the same time. Mixes of iterable and not iterable'
+            ' are not supported.'
+        )
--- tests/database/model.py
+++ tests/database/model.py
@@ -1,15 +1,65 @@
+import os
 from sentiment_analysis.database.base import Base
-from sentiment_analysis.database.model import Model
+from sentiment_analysis.database.model import Model, add_model
+from datetime import datetime
 from sqlalchemy import create_engine, inspect
+from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
 
 
-DB_URL = 'sqlite:///test.db'
-ENGINE = create_engine(DB_URL)
-SESSION = sessionmaker(bind=ENGINE)
+DATABASE = {
+    'drivername': 'sqlite',
+    'database': 'test.db'
+}
+ENGINE = create_engine(URL.create(**DATABASE))
+SESSION = sessionmaker(bind=ENGINE)()
 
 
 def test_create_model_table():
     Base.metadata.create_all(ENGINE, checkfirst=True)
     insp = inspect(ENGINE)
     assert Model.__tablename__ in insp.get_table_names()
+
+
+def test_add_model_single_record():
+    record = {
+        'model_name': 'logreg',
+        'model_version': 1,
+        'model_desc': 'some desc',
+        'model_ref': 'here.joblib',
+        'train_size': 1,
+        'trained_at_dt': datetime(2000, 1, 31)
+    }
+    add_model(SESSION, **record)
+    records = SESSION.query(Model).all()
+    assert len(records) == 1
+    assert (
+        (records[0].model_id == 1)
+        & (records[0].model_name == record['model_name'])
+        & (records[0].model_version == record['model_version'])
+        & (records[0].model_desc == record['model_desc'])
+        & (records[0].model_ref == record['model_ref'])
+        & (records[0].train_size == record['train_size'])
+        & (records[0].trained_at_dt == record['trained_at_dt'])
+    )
+
+
+def test_add_model_multiple_records_same():
+    records = {
+        'model_name': ['logreg', 'logreg'],
+        'model_version': [2, 2],
+        'model_desc': ['some desc', 'some desc'],
+        'model_ref': ['here.joblib', 'here.joblib'],
+        'train_size': [1, 1],
+        'trained_at_dt': [datetime(2000, 1, 31), datetime(2000, 1, 31)]
+    }
+    Base.metadata.create_all(ENGINE, checkfirst=True)
+    add_model(SESSION, **records)
+    records_back = SESSION.query(Model).filter(Model.model_version == 2).all()
+    # assert len(records_back) == 1
+    assert (records_back[0].model_version == 2) and (records_back[1].model_version == 2)
+
+
+def test_dummy():
+    path = os.path.abspath(DATABASE['database'])
+    os.remove(path)
