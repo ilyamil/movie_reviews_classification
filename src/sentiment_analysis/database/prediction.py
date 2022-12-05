07144c097d113b0ec70b8a2cafeb8461d3cf011e
imilyutin <idmilyutin@gmail.com>
2022-12-06 00:39:28 +0400

    added tests for prediction table

--- src/sentiment_analysis/database/prediction.py
+++ src/sentiment_analysis/database/prediction.py
@@ -1,5 +1,5 @@
-from typing import List
-from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
+from typing import List, Union, Iterable
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sentiment_analysis.database.base import Base
 
 
@@ -10,15 +10,70 @@ class Prediction(Base):
     tweet_id = Column(Integer, ForeignKey('tweet.tweet_id'))
     label = Column(String)
     score = Column(Float)
-    calculated_at_dt = Column(DateTime)
 
 
 def add_prediction(
     session,
-    prediction_id: List[int],
-    model_id: List[int],
-    tweet_id: List[int],
-    label: List[str],
-    score: List[float]
+    model_id: Union[List[int], int],
+    tweet_id: Union[List[int], int],
+    label: Union[List[str], str],
+    score: Union[List[float], float]
 ):
-    pass
+    args = [model_id, tweet_id, label, score]
+    if all(
+            isinstance(arg, Iterable) and not isinstance(arg, str)
+            for arg in args
+            ):
+        existed_prediction = (
+            session.query(Prediction)
+            .filter(
+                Prediction.model_id.in_(model_id),
+                Prediction.tweet_id.in_(tweet_id)
+            )
+            .all()
+        )
+        existed_prediction_ = {
+            (el.model_id, el.tweet_id, el.label, el.score)
+            for el in existed_prediction
+        }
+        already_added = set()
+        records = set()
+        for mid, tid, l, s in zip(*args):
+            if (mid, tid, l, s) in existed_prediction_\
+               or (mid, tid, l, s) in already_added:
+                continue
+
+            record = Prediction(model_id=mid, tweet_id=tid, label=l, score=s)
+            records.add(record)
+            already_added.add(record)
+
+        if len(records) > 0:
+            session.add_all(records)
+            session.commit()
+    elif all(
+            not isinstance(arg, Iterable) or isinstance(arg, str)
+            for arg in args
+            ):
+        existed_prediction = (
+            session.query(Prediction)
+            .filter(
+                Prediction.model_id == model_id,
+                Prediction.tweet_id == tweet_id
+            )
+            .all()
+        )
+        if len(existed_prediction) == 0:
+            record = Prediction(
+                model_id=model_id,
+                tweet_id=tweet_id,
+                label=label,
+                score=score
+            )
+            session.add(record)
+            session.commit()
+    else:
+        raise ValueError(
+            'All arguments (except session) must be either iterable or not'
+            ' iterable at the same time. Mixes of iterable and not iterable'
+            ' are not supported.'
+        )
--- tests/database/prediction.py
+++ tests/database/prediction.py
@@ -1,9 +1,8 @@
 import os
 from sentiment_analysis.database.base import Base
 from sentiment_analysis.database.prediction import Prediction, add_prediction
-from sentiment_analysis.database.model import Model
-from sentiment_analysis.database.tweet import Tweet
-from datetime import datetime
+from sentiment_analysis.database.model import Model # noqa
+from sentiment_analysis.database.tweet import Tweet # noqa
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
@@ -21,6 +20,73 @@ def test_create_prediction_table():
     Base.metadata.create_all(ENGINE, checkfirst=True)
     insp = inspect(ENGINE)
     assert Prediction.__tablename__ in insp.get_table_names()
+
+
+def test_add_prediction_single_record():
+    record = {
+        'model_id': 1,
+        'tweet_id': 1,
+        'label': 'positive',
+        'score': 0.99
+    }
+    add_prediction(SESSION, **record)
+    q = SESSION.query(Prediction).all()
+    assert len(q) == 1
+    assert (
+        (q[0].model_id == record['model_id'])
+        & (q[0].tweet_id == record['tweet_id'])
+        & (q[0].label == record['label'])
+        & (q[0].score == record['score'])
+    )
+
+
+def test_add_prediction_single_record_twice():
+    record = {
+        'model_id': 2,
+        'tweet_id': 1,
+        'label': 'positive',
+        'score': 0.99
+    }
+    add_prediction(SESSION, **record)
+    add_prediction(SESSION, **record)
+    q = (
+        SESSION.query(Prediction)
+        .filter(Prediction.model_id == record['model_id'])
+        .all()
+    )
+    assert len(q) == 1
+
+
+def test_add_prediction_multiple_records_same():
+    record = {
+        'model_id': [1, 1],
+        'tweet_id': [1, 1],
+        'label': ['positive', 'positive'],
+        'score': [0.99, 0.99]
+    }
+    add_prediction(SESSION, **record)
+    q = (
+        SESSION.query(Prediction)
+        .filter(Prediction.model_id == 1)
+        .all()
+    )
+    assert len(q) == 1
+
+
+def test_add_prediction_multiple_records_different():
+    record = {
+        'model_id': [3, 3],
+        'tweet_id': [10, 20],
+        'label': ['positive', 'positive'],
+        'score': [0.99, 0.99]
+    }
+    add_prediction(SESSION, **record)
+    q = (
+        SESSION.query(Prediction)
+        .filter(Prediction.model_id == 3)
+        .all()
+    )
+    assert len(q) == 2
 
 
 def test_dummy():
